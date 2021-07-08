83b0f4b0b900fb593836dce133c9f48013a86ad0
raniys <raniys@rabbos.com>
2021-07-08 16:21:54 +0800

    improve pre-commit and format codes

--- src/tdd/fizz_buzz.py
+++ src/tdd/fizz_buzz.py
@@ -1,15 +1,15 @@
 def fizz_buzz(n: int):
-    return [value_generate(i) for i in range(1, n+1)]
+    return [value_generate(i) for i in range(1, n + 1)]
 
 
 def value_generate(index: int):
     item = ''
     if index % 3 == 0 and index % 5 == 0:
-        item = "FizzBuzz"
+        item = 'FizzBuzz'
     elif index % 3 == 0:
-        item = "Fizz"
+        item = 'Fizz'
     elif index % 5 == 0:
-        item = "Buzz"
+        item = 'Buzz'
     else:
         item = str(index)
     return item
--- tests/test_fizzbuzz/test_fizz_buzz.py
+++ tests/test_fizzbuzz/test_fizz_buzz.py
@@ -14,5 +14,6 @@ def test_fizz_buzz():
     assert fizz_buzz(2) == ['1', '2']
     assert fizz_buzz(3) == ['1', '2', 'Fizz']
     assert fizz_buzz(5) == ['1', '2', 'Fizz', '4', 'Buzz']
-    assert fizz_buzz(15) == ["1", "2", "Fizz", "4", "Buzz", "Fizz",
-                             "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+    assert fizz_buzz(15) == [
+        '1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz'
+    ]
